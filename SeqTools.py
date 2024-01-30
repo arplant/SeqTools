e16f59b4118ff1a7149d721a28faa55b392ef34c
arplant <alastairrobertplant@gmail.com>
2024-01-30 15:31:03 +0000

    Update SeqTools.py
    Completed translate method
    - Codon parsing now internal method
    - Could replace if statement for codon length with for, range

--- SeqTools.py
+++ SeqTools.py
@@ -8,26 +8,13 @@ Sequence tools
 """
 
 class Sequence:
-    def __init__(self, seq):
+    def __init__(self, seq, codon_table):
         self.seq = seq
+        self.codon_table = codon_table
     
     def printSeq(self):
         print(self.seq)
-    
-    def translate(self):
-        seq_idx = 0
-        codon = []
-        translation = []
-        while seq_idx < len(seq):
-            if len(codon) < 3:
-                codon.append(seq[seq_idx])
-            else:
-                residue = ''.join(str(base) for base in codon)
-                translation.append(residue)
-                codon.clear()
-            seq_idx += 1
-        return translation
-    
+
     def reverse(self):
         rev = []
         seqLength = len(seq) - 1
@@ -35,12 +22,103 @@ class Sequence:
             rev.append(seq[idx])
         reverseSeq = ''.join(str(base) for base in rev)
         return reverseSeq
-            
+    
+    def translate(self):
         
+        def _codonise(self):
+            seq_idx = 0
+            codon = []
+            codon_list = []
+            while seq_idx < len(seq):
+                if len(codon) < 3:
+                    codon.append(seq[seq_idx])
+                else:
+                    residue = ''.join(str(base) for base in codon)
+                    codon_list.append(residue)
+                    codon.clear()
+                seq_idx += 1
+            return codon_list
+        
+        codons = _codonise(seq)
+        aa_residues = []
+        for i in codons:
+            aa_residues.append(table[i])
+        return aa_residues
+    
+    
+        
+
+table = {
+    'tca': 'S',
+    'tcc': 'S',
+    'tcg': 'S',
+    'tct': 'S',
+    'ttc': 'F',
+    'ttt': 'F',
+    'tta': 'L',
+    'ttg': 'L',
+    'tac': 'Y',
+    'tat': 'Y',
+    'taa': '*',
+    'tag': '*',
+    'tgc': 'C',
+    'tgt': 'C',
+    'tga': '*',
+    'tgg': 'W',
+    'cta': 'L',
+    'ctc': 'L',
+    'ctg': 'L',
+    'ctt': 'L',
+    'cca': 'P',
+    'ccc': 'P',
+    'ccg': 'P',
+    'cct': 'P',
+    'cac': 'H',
+    'cat': 'H',
+    'caa': 'Q',
+    'cag': 'Q',
+    'cga': 'R',
+    'cgc': 'R',
+    'cgg': 'R',
+    'cgt': 'R',
+    'ata': 'I',
+    'atc': 'I',
+    'att': 'I',
+    'atg': 'M',
+    'aca': 'T',
+    'acc': 'T',
+    'acg': 'T',
+    'act': 'T',
+    'aac': 'N',
+    'aat': 'N',
+    'aaa': 'K',
+    'aag': 'K',
+    'agc': 'S',
+    'agt': 'S',
+    'aga': 'R',
+    'agg': 'R',
+    'gta': 'V',
+    'gtc': 'V',
+    'gtg': 'V',
+    'gtt': 'V',
+    'gca': 'A',
+    'gcc': 'A',
+    'gcg': 'A',
+    'gct': 'A',
+    'gac': 'D',
+    'gat': 'D',
+    'gaa': 'E',
+    'gag': 'E',
+    'gga': 'G',
+    'ggc': 'G',
+    'ggg': 'G',
+    'ggt': 'G' 
+}
 
 seq = "atgatcatactagactag"
 residues = list(seq)
-mySeq = Sequence(residues)
+
+mySeq = Sequence(residues, table)
 #mySeq.printSeq()
 print(mySeq.reverse())
 print(mySeq.translate())
